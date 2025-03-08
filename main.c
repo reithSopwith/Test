c45c183f0fdb5fbd644488e6d9ea9a8e3e5719e4
Reith Sopwith <reith.sopwith@gmail.com>
2025-03-08 12:46:33 +0000

    Initial Commit

---
+++ main.c
@@ -0,0 +1,8 @@
+//In the program I will be playing with some operators
+#include <stdio.h>
+int main() {
+    int Length = 15;
+    int Breadth = 10;
+    int Area = Length * Breadth;
+    printf("The area of the rectangle is %d\n", Area);
+}
